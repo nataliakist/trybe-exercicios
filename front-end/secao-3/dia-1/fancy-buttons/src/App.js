fe7b623b3a3a639b4c2bae34a65ea5993a609100
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-01-25 15:57:50 -0300

    Exercicio de fixação 2

--- front-end/secao-3/dia-1/fancy-buttons/src/App.js
+++ front-end/secao-3/dia-1/fancy-buttons/src/App.js
@@ -2,21 +2,31 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.button1 = this.button1.bind(this)
+    this.button2 = this.button2.bind(this)
+    this.button3 = this.button3.bind(this)
+  }
+  button1() {
+    console.log(this)
+    console.log('Clicou no botão 1!')
+  }
+  button2() {
+    console.log(this)
+    console.log('Clicou no botão 2!')
+  }
+  button3() {
+    console.log(this)
+    console.log('Clicou no botão 3!')
+  }
+
   render() {
-    function button1() {
-      console.log('Clicou no botão 1!')
-    }
-    function button2() {
-      console.log('Clicou no botão 2!')
-    }
-    function button3() {
-      console.log('Clicou no botão 3!')
-    }
     return (
       <>
-        <button onClick={button1}>Botão 1</button>
-        <button onClick={button2}>Botão 2</button>
-        <button onClick={button3}>Botão 3</button>
+        <button onClick={this.button1}>Botão 1</button>
+        <button onClick={this.button2}>Botão 2</button>
+        <button onClick={this.button3}>Botão 3</button>
       </>
     );
   }
