d661ada7580f8da8dadf43613044b181a42740f4
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-05-17 14:38:11 -0300

    Adicionando função para calculo de classificação de situação do IMC

--- back-end/secao-4/dia-1/exercises/bmi.js
+++ back-end/secao-4/dia-1/exercises/bmi.js
@@ -1,7 +1,30 @@
+const readline = require('readline-sync');
+const BMI_MAX_AND_MIN = require('./bmiMaxAndMin')
+
 function bmiCalculator(weight, height) {
-  const result = weight/((height/100)^2);
-  return result;
+  const heightSquared = (height / 100) ** 2
+  const result = weight / heightSquared;
+  
+  return result.toFixed(2);
 };
 
-console.log(bmiCalculator(84, 168))
+function handleBMIResult(result) {
+  const statuses = Object.keys(BMI_MAX_AND_MIN)
+  const statusFind = statuses.find((currentStatus) => {
+    const { minBMI, maxBMI } = BMI_MAX_AND_MIN[currentStatus]
+    if(result >= minBMI && result <= maxBMI) return currentStatus
+  })
+  return statusFind;
+};
+
+function main() {
+  const weight = readline.questionFloat('What\’s your weight(in kg)? ');
+  const height = readline.questionInt('What\’s your weight(in cm)? ')
+  const result = bmiCalculator(weight, height)
+  const statusResult = handleBMIResult(result)
+  console.log(result);
+  console.log(statusResult);
+  }
+
+main();
 
