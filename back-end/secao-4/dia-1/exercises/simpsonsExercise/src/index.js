367d2b1521d2382b04120102e953e479435f33f5
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-05-17 16:43:10 -0300

    Adaptando a função de criar novo personagem para adicionar o personagem Nelson Muntz no novo arquivo simpsonFamily

--- back-end/secao-4/dia-1/exercises/simpsonsExercise/src/index.js
+++ back-end/secao-4/dia-1/exercises/simpsonsExercise/src/index.js
@@ -1,13 +1,16 @@
-const { readData, writeNewData, searchCharacter } = require('./utils/fsUtils');
+const { readData, writeNewData, searchCharacter, deleteCharacter, writeNewFile } = require('./utils/fsUtils');
 const readline = require('readline-sync')
 
 async function main() {
   // await readData();
   // const name = readline.question('What\'s the character name? ')
   // writeNewData({ name });
-  const characterId = readline.questionInt('What\'s the character id you want to look for? ')
-  const result = await searchCharacter(characterId);
-  console.log(result);
+  // const characterId = readline.questionInt('What\'s the character id you want to look for? ')
+  // const result = await searchCharacter(characterId);
+  // console.log(result);
+  // await deleteCharacter();
+  // await writeNewFile();
+  await writeNewData();
 }
 
 main()
--- back-end/secao-4/dia-1/exercises/simpsonsExercise/src/utils/fsUtils.js
+++ back-end/secao-4/dia-1/exercises/simpsonsExercise/src/utils/fsUtils.js
@@ -2,6 +2,7 @@ const fs = require('fs').promises;
 const path = require('path')
 
 const SIMPSONS_DATA_PATH = '../../data/simpsons.json';
+const SIMPSONS_FAMILY_PATH = '../../data/simpsonFamily.json'
 
 async function readData() {
   try {
@@ -15,12 +16,13 @@ async function readData() {
   }
 }
 
-async function writeNewData(newData) {
+async function writeNewData() {
   try {
-    const oldData = await readData();
-    const fullData = [...oldData, { id: oldData.length + 1, ...newData}];
+    const data = await fs.readFile(path.resolve(__dirname, SIMPSONS_FAMILY_PATH), 'utf-8');
+    const oldData = JSON.parse(data);
+    const fullData = [...oldData, {id: 8, name:"Nelson Muntz"}];
     const stringifyData = JSON.stringify(fullData);
-    await fs.writeFile(path.resolve(__dirname, SIMPSONS_DATA_PATH), stringifyData);
+    await fs.writeFile(path.resolve(__dirname, SIMPSONS_FAMILY_PATH), stringifyData);
     console.log('New character successfully added')
   } catch(e) {
     console.error(`Erro: ${e.message}`)
@@ -36,8 +38,26 @@ async function searchCharacter(characterId) {
   return character;
 }
 
+async function deleteCharacter() {
+  const data = await readData();
+  const newData = data.filter((character) => Number(character.id) !== 6 && Number(character.id) !== 10)
+  const stringify = JSON.stringify(newData);
+  await fs.writeFile(path.resolve(__dirname, SIMPSONS_DATA_PATH), stringify);
+  console.log('Personagens removidos com sucesso');
+}
+
+async function writeNewFile() {
+  const data = await readData();
+  const newData = data.filter((character) => Number(character.id) <= 5)
+  const stringify = JSON.stringify(newData);
+  await fs.writeFile(path.resolve(__dirname, '../../data/simpsonFamily.json'), stringify);
+  console.log('Novo arquivo criado com sucesso');
+}
+
 module.exports = {
   readData,
   writeNewData,
   searchCharacter,
+  deleteCharacter,
+  writeNewFile,
 };
