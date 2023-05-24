4b0e037434937cc9ef46cb96874bee8b6d175259
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-05-24 15:58:26 -0300

    Exercícios do dia, praticando criação de endpoints de API, exercícios 1 ao 8

---
+++ back-end/secao-4/dia-2/moviesAPI/src/app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+
+const fs = require('fs').promises;
+const path = require('path');
+
+const moviesPath = path.resolve(__dirname, './movies.json');
+
+const app = express();
+
+app.use(express.json())
+
+async function readFile() {
+  try {
+    const data = await fs.readFile(moviesPath, 'utf-8')
+    return JSON.parse(data);
+  } catch (e) {
+    console.error(`Não é possível ler o arquivo: ${e.message}`);
+  }
+}
+
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await readFile();
+    res.status(200).json({ movies });
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+app.get('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const movies = await readFile();
+    const movie = movies.find((element) => element.id === Number(id));
+    res.status(200).json({ movie });
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+app.post('/movies', async (req, res) => {
+  try {
+    const movies = await readFile();
+    const newMovie = { id:movies[movies.length - 1].id + 1, ...req.body }
+    const allMovies = JSON.stringify([...movies, newMovie])
+    await fs.writeFile(moviesPath, allMovies);
+    res.status(201).json({ movie: newMovie })
+  } catch (err) {
+    res.status(500).send({ message: err.message })
+  }
+})
+
+app.put('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const { movie, price } = req.body;
+  try {
+    const movies = await readFile();
+    const index = movies.findIndex((element) => element.id === +id)
+    if (!index) {
+      res.status(404).json({ message: 'Movie not found' });
+    }
+    movies[index] = { id: Number(id), movie, price };
+    const updatedMovies = JSON.stringify(movies, null, 2)
+    await fs.writeFile(moviesPath, updatedMovies);
+    res.status(200).json({ movie: movies[index] })
+  } catch (err) {
+    res.status(500).send({ message: err.message })
+  }
+})
+
+module.exports = app;
