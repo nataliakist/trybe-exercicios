4a1188232f353cac9619826d013bfff5abb87066
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-06-03 19:19:00 -0300

    Finalizada api trybecash desenvolvida durante a aula do course

--- back-end/secao-4/dia-5/trybecash-api/src/server.js
+++ back-end/secao-4/dia-5/trybecash-api/src/server.js
@@ -1,7 +1,13 @@
+
+require('dotenv').config();
 const app = require('./app');
+const connection = require('./db/connection');
 
 const PORT = 3001;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
+  const [result] = await connection.execute('SELECT 1');
+
+  if (result) console.log('MySQL Connection Ok!')
 });
