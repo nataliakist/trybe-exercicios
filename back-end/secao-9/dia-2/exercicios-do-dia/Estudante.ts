993c4da845fa5f58654dea44f8d90d8e4cff643a
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-08-21 21:02:12 -0300

    Exercícios finalizados

--- back-end/secao-9/dia-2/exercicios-do-dia/Estudante.ts
+++ back-end/secao-9/dia-2/exercicios-do-dia/Estudante.ts
@@ -32,7 +32,7 @@ class Person {
   
   private validateBirthDate(date: Date):void {
     if (date.getTime() > Date.now()) throw new Error('Até o momento, é impossível que alguém venha do futuro, informe a data de nascimento correta');
-    if (this.getAge() > 120) throw new Error('Impossível que a pessoa tenha mais de 120 anos, informe a data de nascimento correta')
+    if (this.getAge() > 120) throw new Error('Impossível que a pessoa seja uma múmia, informe a data de nascimento correta')
   }
 
   private validatePerson(): void {
@@ -88,8 +88,93 @@ class Student extends Person {
   }
 
   generateEnrollment() {
-    const randomStr = String(Math.random()).substr(2, 13)
+    const randomStr = String(Math.random() + 1).substr(2, 13)
     return 'STU' + randomStr
+  }
+}
+
+interface Employee {
+  registration: string,
+  salary: number,
+  admissionDate: Date,
+
+  generateRegistration() : string;
+}
+
+class Subject {
+  private _name: string;
+  constructor(nameValue:string) {
+    this.verifyName(nameValue);
+    this._name = nameValue;
+    }
+
+  get name():string {
+    return this._name;
+  }
+
+  set name(value:string) {
+    this.verifyName(value);
+    this._name = value;
+  }
+
+  verifyName(value:string):void {
+    if (value.length < 3) throw new Error('Nome deve possuir ao menos três caracteres')
+  }
+}
+
+class Teacher extends Person implements Employee{
+  private _subject: Subject;
+  private _registration = String();
+  private _admissionDate: Date;
+
+  constructor(name: string, birthDate: Date, private _salary: number, subject: Subject) {
+    super(name, birthDate);
+
+    this._subject = subject;
+    this.salary = _salary;
+    this._admissionDate = new Date();
+    this.registration = this.generateRegistration();
+  }
+
+  get subject():Subject {
+    return this._subject;
+  }
+
+  set subject(value:Subject) {
+    this._subject = value;
+  }
+
+  get registration(): string {
+    return this._registration;
+  }
+
+  set registration(value: string) {
+    if (value.length < 16) throw new Error('O registro deve possuir no mínimo 16 caracteres.');
+
+    this._registration = value;
+  }
+
+  get salary() : number {
+    return this._salary;
+  }
+
+  set salary(value:number) {
+    if (value <= 0) throw new Error('O salário deve ser maior que zero');
+    this._salary = value;
+  }
+
+  get admissionDate() : Date {
+    return this._admissionDate;
+  }
+
+  set admissionDate(value:Date) {
+    if (value.getTime() > Date.now()) throw new Error('A data de admissão não pode ser no futuro');
+    this._admissionDate = value;
+  }
+
+  generateRegistration(): string {
+    const randomStr = String(Math.random() + 1).substr(2, 13)
+    return 'TEA' + randomStr
   }
 }
 
@@ -108,8 +193,22 @@ lucas.examsGrades = [25, 20, 25, 23];
 jessica.assignmentsGrades = [50, 45];
 tamires.assignmentsGrades = [47, 42];
 
+const mat = new Subject('Matématica');
+const hist = new Subject('História');
+const fil = new Subject('Filosofia');
+
+const marta = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, mat);
+const joao = new Teacher('João Antônio da Costa', new Date('1982/04/21'), 2000, hist);
+const lucio = new Teacher('Lucio Teixeira', new Date('1986/01/29'), 2000, fil);
+
 console.log(carolina);
 console.log(lucas);
 console.log(jessica);
 console.log(tamires);
 console.log(fernando);
+console.log(mat.name);
+console.log(hist.name);
+console.log(fil.name);
+console.log(marta);
+console.log(joao);
+console.log(lucio);
