46c8ab6da7dcabffbf605520af579f4acd3f5673
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-10-23 19:09:08 -0300

    Another way of counting students in time instant

--- ciencia-da-computacao/secao-3/dia-2/exercicio4.py
+++ ciencia-da-computacao/secao-3/dia-2/exercicio4.py
@@ -1,14 +1,21 @@
-def students_counter(entry, departure, instant):
+def students_counter(arrivals, departures, time_instant):
   counter = 0
-  for i in range(len(entry)):
-    if entry[i] <= instant <= departure[i]:
+  for i in range(len(arrivals)):
+    if arrivals[i] <= time_instant <= departures[i]:
       counter += 1
   return counter
 
+
+def students_in_instant(arrivals, departures, time_instant):
+    result = sum(
+        arrival <= time_instant <= departure
+        for arrival, departure in zip(arrivals, departures)
+    )
+    return result
 
 entradas = [1, 2, 3, 4]
 saidas = [3, 2, 7, 5]
 instante_buscado = 2
 resultado: 1
 
-print(students_counter(entradas, saidas, instante_buscado))
+print(students_in_instant(entradas, saidas, instante_buscado))
