99734b0f6cc117943ddaf1252381dfe4fdc594dc
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-10-23 23:55:56 -0300

    Refatorando função

--- ciencia-da-computacao/secao-3/dia-3/bonus_exercise2.py
+++ ciencia-da-computacao/secao-3/dia-3/bonus_exercise2.py
@@ -2,26 +2,41 @@ from doubly_linked_list import DoublyLinkedList
 from doubly_node import DoublyNode
 
 def delete_duplicates(doubly_linked_list):
+    # create new dummy node and point to head
     new_node = DoublyNode(doubly_linked_list.head.value)
-    new_node.next = doubly_linked_list.head.next
+
+    # declare prev and point on dummy
     prev = new_node
+
+    # declare cur and point on dummy.next
     cur = doubly_linked_list.head.next
+
     new_list = DoublyLinkedList() 
 
-    while cur.next:
-        if cur.next.value == cur.value:
+    # iterate untill head reaches end of linked list
+    while cur:
+        # if we find duplicate:
+        if cur.next and cur.next.value == cur.value:
+            # we iterate until we have duplicates:
             while cur.next and cur.next.value == cur.value:
+                # move cur to next duplicate
                 cur = cur.next
-            cur.next, cur = None, cur.next
-            prev.next = cur
+            # remove all duplicates by pointing prev.next to head.next
+            prev.next = cur.next
             
+        # if not duplicate:    
         else:
-            prev = cur
-            cur = cur.next
+            # move prev pointer
+            prev = prev.next
             new_list._length += 1
+            
+        # move head pointer
+        cur = cur.next
 
-    
+    # removing the tail from the counter
+    new_list._length -= 1
 
+    # adding the node to the new list
     new_list.head.next = new_node.next
     return new_list
 
