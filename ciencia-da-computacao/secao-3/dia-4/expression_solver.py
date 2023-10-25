02947f06fa4bdc5d45c665c29d16ba3434f2d95f
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-10-25 00:22:33 -0300

    Exercicio 6
    Adicionando as funções de subtração e divisão ao expression_solver

--- ciencia-da-computacao/secao-3/dia-4/expression_solver.py
+++ ciencia-da-computacao/secao-3/dia-4/expression_solver.py
@@ -13,10 +13,25 @@ def solve_expression(expr):
             # multiply operation
             result = stack.pop() * stack.pop()
             stack.push(result)
+        elif token == '-':
+            op1 = stack.pop()
+            op2 = stack.pop()
+            result = op2 - op1
+            stack.push(int(result))
+        elif token == '/':
+            op1 = stack.pop()
+            op2 = stack.pop()
+            result = op2 / op1
+            stack.push(int(result))
         else:
             # add number operation
             stack.push(int(token))
     return stack.pop()
 
 
-print(solve_expression("5 10 + 3 *"))
+print(solve_expression("5 10 + 3 *")) # Saída 45
+print(solve_expression('5 10 + 30 5 / -')) # 9
+print(solve_expression('10 5 30 * 30 / 2 * +')) # 20
+print(solve_expression('5 10 * 30 - 4 5 * 10 - +')) # 30
+print(solve_expression('30 10 / 5 + 5 10 + *')) # 120
+print(solve_expression('50 10 * 2 / 5 * 5 /')) # 250
