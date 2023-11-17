8adaf7477d1ba1152b83d9eb4f8359a4f1b19d8e
Natália Lauschner Kist <natalialkist@hotmail.com>
2023-11-17 15:25:54 -0300

    Atributos definidos como privados e adicionados getters e setters

--- eletiva_java/secao-2/dia-1/Exercicios/src/Carro.java
+++ eletiva_java/secao-2/dia-1/Exercicios/src/Carro.java
@@ -2,9 +2,9 @@
  * The Class Carro.
  */
 public class Carro {
-  String marca;
-  String modelo;
-  int ano;
+  private String marca;
+  private String modelo;
+  private int ano;
 
   /**
    * Instantiates a new Carro.
@@ -21,5 +21,29 @@ public class Carro {
 
   public void exibirInformacoes() {
     System.out.println("Marca: " + marca + ", Modelo: " + modelo + ", Ano: " + ano);
+  }
+
+  public String getMarca() {
+    return marca;
+  }
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
+  public int getAno() {
+    return ano;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
   }
 }
